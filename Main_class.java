0d4d4fa87c7000b115c0a5673d00ab26506b15c2
Axiino <max_rymar@mail.ru>
2016-06-25 20:36:57 +0300

    Класс граф. Генерация графа.
    Добавлен класс граф с функцией для генерации.

---
+++ Main_class.java
@@ -0,0 +1,15 @@
+package Bridges;
+
+/**
+ * Created by Максим on 24.06.2016.
+ */
+public class Main_class {
+    public static void main(String[] args) {
+        graph G = new graph(5);
+
+        G.initialisation(5,graph.density.rare);
+
+        G.show();
+
+    }
+}
