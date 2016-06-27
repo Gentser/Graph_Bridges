195093ff0b40dbc498739ccd0633b5d09b19580c
Axiino <max_rymar@mail.ru>
2016-06-27 19:36:14 +0300

    Update Main_class.java

--- Main_class.java
+++ Main_class.java
@@ -6,10 +6,15 @@ package Bridges;
 public class Main_class {
     public static void main(String[] args) {
         graph G = new graph(5);
-
+        
+         System.out.println("V = " + G.V + ", E = " + G.E);
         G.initialisation(5,graph.density.rare);
-
+        
+        
         G.show();
+        
+        Algorithm T = new Algorithm();
+        T.DFS_bridge(G);
 
     }
 }
